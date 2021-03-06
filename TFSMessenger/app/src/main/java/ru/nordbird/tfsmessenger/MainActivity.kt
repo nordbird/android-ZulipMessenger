c9f7048404b8e3964af6096226b1a6925258a6e0
NevedrovAS <nevedrov@mineframe.ru>
2021-03-06 21:36:19 +0300

    Добавление кастомной View для отображения Emoji реакции и кол-ва реакций

--- TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/MainActivity.kt
+++ TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.nordbird.tfsmessenger
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.nordbird.tfsmessenger.databinding.ActivityMainBinding
+import ru.nordbird.tfsmessenger.extensions.spToPx
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.evSample.emojiCount = 8
+        binding.evSample.emojiCode = 0x1F44D
+        binding.evSample.textSize = spToPx(20F)
+        binding.evSample.textColor = Color.GREEN
+        binding.evSample.isSelected = true
+
+        binding.evSample.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
     }
 }
