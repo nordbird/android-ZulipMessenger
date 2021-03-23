1f493181450e06db5912a8d41b837107bc6d709e
NevedrovAS <nevedrov@mineframe.ru>
2021-03-23 17:25:55 +0300

    Реализация экрана детали профиля

--- TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/ui/profile/ProfileFragment.kt
+++ TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/ui/profile/ProfileFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.nordbird.tfsmessenger.R
+import ru.nordbird.tfsmessenger.data.DataGenerator
 import ru.nordbird.tfsmessenger.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -12,13 +14,23 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+    private val currentUser = DataGenerator.getCurrentUser()
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        initUI()
+        setupUser()
         return binding.root
+    }
+
+    private fun initUI() {
+        binding.ivProfileAvatar.clipToOutline = true
+    }
+
+    private fun setupUser() {
+        binding.tvProfileName.text = currentUser.name
+        binding.tvProfileOnline.text = if (currentUser.isOnline) getText(R.string.profile_online) else getText(R.string.profile_offline)
+        binding.tvProfileStatus.text = getText(R.string.profile_status)
+        binding.ivProfileAvatar.setImageResource(R.drawable.avatar)
     }
 }
