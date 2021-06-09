605d7c4ab6f06ca8c574c3f9a2ce5e522fc8f6bf
NevedrovAS <nevedrov@mineframe.ru>
2021-06-09 10:58:44 +0300

    Удаление auth-токена

--- TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/data/api/ZulipAuth.kt
+++ TFSMessenger/app/src/main/java/ru/nordbird/tfsmessenger/data/api/ZulipAuth.kt
@@ -1,7 +1,7 @@
 package ru.nordbird.tfsmessenger.data.api
 
 object ZulipAuth {
-    const val AUTH_ID = 402231
-    const val AUTH_EMAIL = "i-nev@mail.ru"
-    const val AUTH_KEY = "WnuB2s2PVC9LCGCKMUwTNAzcXFVpHRHu"
+    const val AUTH_ID = 0
+    const val AUTH_EMAIL = ""
+    const val AUTH_KEY = ""
 }
